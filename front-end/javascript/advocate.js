afa5c3e77a3c68a7a8e9950fbd32a153715c2da9
Ruchit Raushan <ruchitraushan@gmail.com>
2023-09-28 21:34:16 +0530

    login options added

---
+++ front-end/javascript/advocate.js
@@ -0,0 +1,18 @@
+let loginPage = document.getElementById("login");
+let signupPage = document.getElementById("signup");
+
+signupPage.style.display = "none";
+function loginToggle() {
+  loginPage.style.display = "flex";
+  signupPage.style.display = "none";
+}
+function signupToggle() {
+  signupPage.style.display = "flex";
+  loginPage.style.display = "none";
+}
+// function loginCancelToggle() {
+//   loginPage.style.display = "none";
+// }
+// function signupCancelToggle() {
+//   signupPage.style.display = "none";
+// }
